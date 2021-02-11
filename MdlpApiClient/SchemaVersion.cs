9d181c6d75300c1fbfba2dc9fdfc9d4315d3a636
Nikita Zenkov <cmaker.xtal@hotmail.com>
2021-02-11 15:46:39 +0600

    Add schema Version

---
+++ MdlpApiClient/SchemaVersion.cs
@@ -0,0 +1,13 @@
+namespace MdlpApiClient
+{
+    public class SchemaVersion
+    {
+        // ReSharper disable once InconsistentNaming
+        public static string V1_34 = "1.34";
+
+        // ReSharper disable once InconsistentNaming
+        public static string V1_35 = "1.35";
+        
+        public static string Current = V1_35;
+    }
+}
